0e328b1a925aa1f7eaf7815592c71779be3240da
Fidanze <fidanze@gmail.com>
2022-08-10 23:45:37 +0500

    add docstrings and format code

--- three_in_a_row/__init__.py
+++ three_in_a_row/__init__.py
@@ -1,1 +1,2 @@
+'''Python realization of popular mobile game "Three in a row" for CLI'''
 __version__ = '0.1.0'
--- three_in_a_row/crystal.py
+++ three_in_a_row/crystal.py
@@ -1,16 +1,19 @@
+"""Realisation of game crystal by using Enum"""
 import enum
 from termcolor import colored
 
-class Crystal(enum.Enum):
-    # '''Enum for kind of crystals'''
-    STAR = {'symbol':'*', 'color':'yellow'}
-    PLUS = {'symbol':'+', 'color':'blue'}
-    DOLLAR = {'symbol':'$', 'color':'green'}
-    CROSS = {'symbol':'X', 'color':'red'}
 
-    def __init__(self, vals):
-        self.symbol = vals['symbol']
-        self.color = vals['color']
-        
-    def __str__(self):
-        return  colored(f'{self.name}: {self.value["symbol"]}', self.value['color'])
+class Crystal(enum.Enum):
+    """Enum for kind of crystals"""
+
+    STAR = {"symbol": "*", "color": "yellow"}
+    PLUS = {"symbol": "+", "color": "blue"}
+    DOLLAR = {"symbol": "$", "color": "green"}
+    CROSS = {"symbol": "X", "color": "red"}
+
+    def __init__(self, vals:dict[str,str]) -> None:
+        self.symbol = vals["symbol"]
+        self.color = vals["color"]
+
+    def __str__(self) -> str:
+        return colored(f'{self.name}: {self.value["symbol"]}', self.value["color"])
