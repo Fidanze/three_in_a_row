07525d9905b5be7ce8b36440035791ba3cc92565
fidanze <fidanze@gmail.com>
2022-08-18 15:25:38 +0500

    rewrite methods with using get and set item

--- three_in_a_row/gameField.py
+++ three_in_a_row/gameField.py
@@ -39,30 +39,32 @@ class GameField:
             # generate new symbols in None points
             for y in range(self.N):
                 for x in range(self.N):
-                    if self.field[y][x] == None:
-                        self.field[y][x] = choice(list(self.symbol_class))
+                    if self[y][x] == None:
+                        self[y][x] = choice(list(self.symbol_class))
         return count
 
     def fall(self) -> None:
         for x in range(self.N):
             for y in range(self.N - 1, -1, -1):
-                if self.field[y][x] == None:
+                if self[y][x] == None:
                     for y_offset in range(y - 1, -1, -1):
-                        if self.field[y_offset][x] != None:
-                            self.field[y][x], self.field[y_offset][x] = (
-                                self.field[y][x],
-                                self.field[y_offset][x],
+                        if self[y_offset][x] != None:
+                            self[y][x], self[y_offset][x] = (
+                                self[y][x],
+                                self[y_offset][x],
                             )
                             break
 
     def swap(self, y1: int, x1: int, y2: int, x2: int) -> int:
         # checking neighborhood
-        if (x1 == x2 and y1 in (y2 - 1, y2 + 1)) or (y1 == y2 and x1 in (x2 - 1, x2 + 1)):
-            self.field[y1][x1], self.field[y2][x2] = self.field[y2][x2], self.field[y1][x1]
+        if (x1 == x2 and (y1 == y2 - 1 or y1 == y2 + 1)) or (
+            y1 == y2 and (x1 == x2 - 1 or x1 == x2 + 1)
+        ):
+            self[y1][x1], self[y2][x2] = self[y2][x2], self[y1][x1]
             if self._get_triples():
                 return self.remove()
             else:
-                self.field[y2][x2], self.field[y1][x1] = self.field[y1][x1], self.field[y2][x2]
+                self[y2][x2], self[y1][x1] = self[y1][x1], self[y2][x2]
                 return 0
         else:
             raise Exception("Symbols must be neighbors")
@@ -73,26 +75,15 @@ class GameField:
         for y in range(self.N):
             for x in range(self.N):
                 if y >= 2:
-                    if len({self.field[y][x], self.field[y - 1][x], self.field[y - 2][x]}) == 1:
-                        to_remove.update(((y, x), (y - 1, x), (y - 2, x)))
+                    if self[y][x] == self[y - 1][x] == self[y - 2][x]:
+                        to_remove.add((y, x))
+                        to_remove.add((y - 1, x))
+                        to_remove.add((y - 2, x))
                 if x >= 2:
-                    if len({self.field[y][x], self.field[y][x - 1], self.field[y][x - 2]}) == 1:
-                        to_remove.update(((y, x), (y, x - 1), (y, x - 2)))
-
-        # # horizontal checking
-        # for row_ind, row in enumerate(self.field):
-        #     for el_ind, el in enumerate(row):
-        #         if el_ind >= 2:
-        #             if el == row[el_ind - 1] == row[el_ind - 2]:
-        #                 for i in range(el_ind - 2, el_ind + 1):
-        #                     to_remove.add((row_ind, i))
-        # # vertical checking
-        # for row_ind, row in enumerate(self.field):
-        #     for el_ind, el in enumerate(row):
-        #         if row_ind >= 2:
-        #             if el == self.field[row_ind - 1][el_ind] == self.field[row_ind - 2][el_ind]:
-        #                 for i in range(row_ind - 2, row_ind + 1):
-        #                     to_remove.add((i, el_ind))
+                    if self[y][x] == self[y][x - 1] == self[y][x - 2]:
+                        to_remove.add((y, x))
+                        to_remove.add((y, x - 1))
+                        to_remove.add((y, x - 2))
         return to_remove
 
     def __str__(self) -> str:
